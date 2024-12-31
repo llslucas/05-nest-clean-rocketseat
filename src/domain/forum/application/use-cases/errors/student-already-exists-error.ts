0db6eb74e047cb90c38ee7770edbdca4e4de70c7
Lucas Souza <145349067+llslucas@users.noreply.github.com>
2024-12-31 16:30:36 -0300

    Implemented error handling on authenticate, create account and fetch recent questions controllers

--- src/domain/forum/application/use-cases/errors/student-already-exists-error.ts
+++ src/domain/forum/application/use-cases/errors/student-already-exists-error.ts
@@ -2,6 +2,6 @@ import { UseCaseError } from "@/core/types/use-case-error";
 
 export class StudentAlreadyExistsError extends Error implements UseCaseError {
   constructor(identifier: string) {
-    super(`Student ${identifier} address already exists.`);
+    super(`Student ${identifier} already exists.`);
   }
 }
