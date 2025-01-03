4e52aec2035c3592d1c30d3eb1775bf2f56fde41
Lucas Souza <145349067+llslucas@users.noreply.github.com>
2025-01-03 15:53:33 -0300

    create answer question controller

---
+++ src/infra/http/controllers/answer-question.controller.e2e-spec.ts
@@ -0,0 +1,62 @@
+import { AppModule } from "@/infra/app.module";
+import { DatabaseModule } from "@/infra/database/database.module";
+import { PrismaService } from "@/infra/database/prisma/prisma.service";
+import { INestApplication } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { Test } from "@nestjs/testing";
+import request from "supertest";
+import { QuestionFactory } from "test/factories/make-question";
+import { StudentFactory } from "test/factories/make-student";
+
+describe("Answer question (E2E)", () => {
+  let app: INestApplication;
+  let studentFactory: StudentFactory;
+  let questionFactory: QuestionFactory;
+  let prisma: PrismaService;
+  let jwt: JwtService;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, QuestionFactory],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    studentFactory = moduleRef.get(StudentFactory);
+    questionFactory = moduleRef.get(QuestionFactory);
+    prisma = moduleRef.get(PrismaService);
+    jwt = moduleRef.get(JwtService);
+
+    await app.init();
+  });
+
+  test("[POST] /questions/:questionId/answers", async () => {
+    const user = await studentFactory.makePrismaStudent();
+    const question = await questionFactory.makePrismaQuestion({
+      authorId: user.id,
+    });
+
+    const accessToken = jwt.sign({ sub: user.id.toString() });
+
+    const response = await request(app.getHttpServer())
+      .post(`/questions/${question.id}/answers`)
+      .set("Authorization", `Bearer ${accessToken}`)
+      .send({
+        content: "Question content",
+      });
+
+    expect(response.statusCode).toBe(201);
+
+    const questionOnDatabase = await prisma.answer.findFirst({
+      where: {
+        content: "Question content",
+      },
+    });
+
+    expect(questionOnDatabase).toBeTruthy();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
